ebbf344166c344a04424e0ceec18092d38cfa4fd
vlad0004p <vlad0004@hz.nl>
2023-10-05 09:43:00 +0200

    Remove unwanted files

--- index/aside_bar.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.display = "block";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("mySidenav").style.display = "none";
-}
